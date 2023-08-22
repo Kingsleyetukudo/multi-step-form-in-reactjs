84699aa42b9f43f582d333ec0c068ce3f908ee78
Kingsleyetukudo <kingsleyj90@gmail.com>
2023-08-22 22:29:53 +0100

    card active state done

--- src/components/SelectPlan.jsx
+++ src/components/SelectPlan.jsx
@@ -10,34 +10,15 @@ function PersonalForm() {
   const [subHeading, setSubHeading] = useState(
     "Ypu have the option of monthly or yearly billing."
   );
-  const [name, setName] = useState("");
-  const [nameError, setNameError] = useState("");
-  const [email, setEmail] = useState("");
-  const [emailError, setEmailError] = useState("");
-  const [phone, setPhone] = useState("");
-  const [phoneError, setPhoneError] = useState("");
+  const [isToggle, setIsToggle] = useState(false);
+  const [isActive, setIsActive] = useState("arcade");
 
-  const checkForm = (e) => {
-    e.preventDefault();
-    console.log("orking");
-    if (name === "") {
-      setNameError("this field is required");
-      console.log(nameError);
-    } else {
-      setNameError("");
-    }
-    if (email === "") {
-      setEmailError("this field is required");
-      console.log(nameError);
-    } else {
-      setEmailError("");
-    }
-    if (phone === "") {
-      setPhoneError("this field is required");
-      console.log(nameError);
-    } else {
-      setPhoneError("");
-    }
+  const checkToggle = (e) => {
+    setIsToggle(e.target.checked);
+  };
+
+  const addActive = (card) => {
+    setIsActive(card);
   };
   return (
     // <section>
@@ -45,46 +26,95 @@ function PersonalForm() {
       <FormHeadings headings={heading} subHeading={subHeading} />
 
       <div className="grid grid-cols-3 gap-4">
-        <div className="border p-4 h-40 flex flex-col rounded-md hover:border-Purplish-blue hover:bg-Magnolia">
+        <div
+          onClick={() => addActive("arcade")}
+          className={`border p-4 flex flex-col gap-8 rounded-md hover:border-Purplish-blue hover:bg-Magnolia ${
+            isActive === "arcade" ? "border-Purplish-blue bg-Magnolia" : ""
+          }`}>
           <div>
             <img src={icon1} alt="Arcade Icon" />
           </div>
           <div className=" mt-auto">
             <p className="text-Marine-blue font-bold text-base">Arcade</p>
-            <p className="text-Cool-gray text-sm">$9/mo</p>
+            {!isToggle ? (
+              <p className="text-Cool-gray text-sm">$9/mo</p>
+            ) : (
+              <div>
+                <p className="text-Cool-gray text-sm">$90/yr</p>
+                <p className="text-Marine-blue text-sm font-medium">
+                  2 months free
+                </p>
+              </div>
+            )}
           </div>
         </div>
-        <div className="border p-4 h-40 flex flex-col rounded-md hover:border-Purplish-blue hover:bg-Magnolia">
+        <div
+          onClick={() => addActive("advanced")}
+          className={`border p-4 flex flex-col gap-8 rounded-md hover:border-Purplish-blue hover:bg-Magnolia ${
+            isActive === "advanced" ? "border-Purplish-blue bg-Magnolia" : ""
+          }`}>
           <div>
-            <img src={icon2} alt="Arcade Icon" />
+            <img src={icon2} alt="Advanced Icon" />
           </div>
           <div className=" mt-auto">
             <p className="text-Marine-blue font-bold text-base">Advaned</p>
-            <p className="text-Cool-gray text-sm">$12/mo</p>
+            {!isToggle ? (
+              <p className="text-Cool-gray text-sm">$12/mo</p>
+            ) : (
+              <div>
+                <p className="text-Cool-gray text-sm">$120/yr</p>
+                <p className="text-Marine-blue text-sm font-medium">
+                  2 months free
+                </p>
+              </div>
+            )}
           </div>
         </div>
-        <div className="border p-4 h-40 flex flex-col rounded-md hover:border-Purplish-blue hover:bg-Magnolia">
+        <div
+          onClick={() => addActive("pro")}
+          className={`border p-4 flex flex-col gap-8 rounded-md hover:border-Purplish-blue hover:bg-Magnolia ${
+            isActive === "pro" ? "border-Purplish-blue bg-Magnolia" : ""
+          }`}>
           <div>
-            <img src={icon3} alt="Arcade Icon" />
+            <img src={icon3} alt="Pro Icon" />
           </div>
           <div className=" mt-auto">
             <p className="text-Marine-blue font-bold text-base">Pro</p>
-            <p className="text-Cool-gray text-sm">$15/mo</p>
+            {!isToggle ? (
+              <p className="text-Cool-gray text-sm">$15/mo</p>
+            ) : (
+              <div>
+                <p className="text-Cool-gray text-sm">$150/yr</p>
+                <p className="text-Marine-blue text-sm font-medium">
+                  2 months free
+                </p>
+              </div>
+            )}
           </div>
         </div>
       </div>
 
-      <div>
-        <p>Monthly</p>
+      <div className="flex items-center justify-center bg-Magnolia p-4 gap-5">
+        <p
+          className={
+            !isToggle ? "font-medium text-Marine-blue" : "text-Cool-gray"
+          }>
+          Monthly
+        </p>
         <label class="switch">
-          <input type="checkbox" />
+          <input type="checkbox" checked={isToggle} onChange={checkToggle} />
           <span class="slider round"></span>
         </label>
-        <p>Yearly</p>
+        <p
+          className={
+            isToggle ? "font-medium text-Marine-blue" : "text-Cool-gray"
+          }>
+          Yearly
+        </p>
       </div>
 
       <div className="mt-auto">
-        <FormBtn onClick={checkForm} />
+        <FormBtn />
       </div>
     </div>
     // </section>
